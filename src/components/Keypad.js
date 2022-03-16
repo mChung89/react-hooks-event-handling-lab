a7ce676cfa82f50aa918a49b4bbdf18743489ee9
Michael Chung <mikechung89@gmail.com>
2022-03-15 21:29:48 -0400

    commit tree 729c14bb1b6d2dbd91f61162815c6c3609fba509

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
 // Code Keypad Component Here
+import React from 'react';
+
+function Keypad() {
+    function enterPassword () {
+        console.log("Entering password...")
+    }
+    return <input onChange={enterPassword} type="password"></input>
+}
+
+export default Keypad;
